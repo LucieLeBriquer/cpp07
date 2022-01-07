d1f149bf9266dabb107d0d255693439db249199f
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-01-07 17:20:07 +0100

    ex01 ok

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,58 +6,71 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 15:03:00 by lle-briq          #+#    #+#             */
-/*   Updated: 2022/01/07 15:06:49 by lle-briq         ###   ########.fr       */
+/*   Updated: 2022/01/07 17:11:41 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 #include <iostream>
-#include <iomanip>
-
-static void     printTitle(std::string title)
-{
-        std::string     toPrint;
-        int     size = 54;
-        int     n;
-
-        toPrint = " " + title + " ";
-        n = toPrint.size();
-        if (n > size)
-        {
-                toPrint = toPrint.substr(0, size - 2);
-                toPrint[size - 4] = '.';
-                toPrint[size - 3] = ' ';
-                n = toPrint.size();
-        }
-        std::cout << std::endl << std::setfill('=') << std::setw(size) << "" << std::endl;
-        std::cout << std::setw(size / 2) << toPrint.substr(0, n / 2);
-        std::cout << toPrint.substr(n / 2, n - n / 2);
-        std::cout << std::setfill('=') << std::setw(size - size / 2 - n + n / 2) << "" << std::endl;
-        std::cout << std::setfill('=') << std::setw(size) << "" << std::endl;
-}
+# define YELLOW "\x1B[33m"
+# define END "\033[0m"
 
 int	main(void)
 {
-	printTitle("SUBJECT TESTS");
+	{
+		std::cout << YELLOW << ">> subject" << END << std::endl;
 
-	int	a = 2;
-	int	b = 3;
+		int	a = 2;
+		int	b = 3;
 
-	::swap(a, b);
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
-	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+		::swap(a, b);
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+		std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
 
-	std::string	c = "chaine1";
-	std::string	d = "chaine2";
+		std::string	c = "chaine1";
+		std::string	d = "chaine2";
 
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
-	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+		std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+	}
+	{
+		std::cout << std::endl << YELLOW << ">> strings" << END << std::endl;
 
-	printTitle("STRINGS");
-	std::cout << "min(c, c) = " << ::min(c, c) << std::endl;
-	std::cout << "max(c, c) = " << ::max(c, c) << std::endl;
+		std::string	a = "retsuko";
+		std::string	b = "fenneko";
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		::swap(a, b);
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+		std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+	}
+	{
+		std::cout << std::endl << YELLOW << ">> char" << END << std::endl;
+
+		char	a = 'a';
+		char	b = 'z';
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		::swap(a, b);
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+		std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+	}
+	{
+		std::cout << std::endl << YELLOW << ">> double" << END << std::endl;
+
+		double	a = 3.4e21;
+		double	b = 3.3e21;
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		::swap(a, b);
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+		std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+	}
 
 	return (0);
 }
